8ae48d41df9d94d9cdcf3d04197d1ec2726ecae8
Ethan <Ethan@172.20.10.2>
2019-09-17 06:55:24 -0700

    House in progress

--- src/House.java
+++ src/House.java
@@ -3,7 +3,34 @@ public class House {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	System.out.println("This is the house that Jack built.");
+	System.out.println("");
+	Malt();
+	System.out.println("");
+	Rat();
+	System.out.println("");
+	Cat();
+	System.out.println("");
+	Dog();
+	System.out.println("");
+	Cow();
+	System.out.println("");
+	Maiden();
+	}
+	public static void Lay() {
+		System.out.println("That lay in the house that Jack built.");
+	}
+	public static void Ate() {
+		System.out.println("That ate the malt");
+	}
+	public static void Malt() {
+		System.out.println("This is the malt");
+		Lay();
+	}
+	public static void Rat() {
+		System.out.println("This is the rat,");
+		Ate();
+		Lay();
 	}
 
 }
